26927e254e527e95f7a4a16811d86e5979db7a6f
Anurag <anrg1576@gmail.com>
2024-08-05 23:27:18 +0530

    modified:   ProjectP1/src/pages/About/About.css 	modified:   ProjectP1/src/pages/About/About.jsx 	modified:   node_modules/.package-lock.json

--- ProjectP1/src/pages/About/About.jsx
+++ ProjectP1/src/pages/About/About.jsx
@@ -1,21 +1,64 @@
-import React from 'react'
-import './About.css'
-import AboutBG from '../../assets/printer-bg1.jpg'
+import React from 'react';
+import './About.css';
+import AboutBG from '../../assets/printer-bg1.jpg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 function About() {
   return (
     <div className="about">
-      <div className="abouthead"><h2>ABOUT US</h2>
+      <div className="abouthead">
+        <h2>ABOUT US</h2>
       </div>
-      <div className="aboutTop"
+      <div
+        className="aboutTop"
         style={{ backgroundImage: `url(${AboutBG})` }}
       ></div>
       <div className="aboutBottom">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, obcaecati! Voluptates vitae consectetur, laborum neque repellendus adipisci quae saepe vero incidunt expedita! Expedita minima eveniet consequuntur laboriosam in iste sapiente.</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab,
+          obcaecati! Voluptates vitae consectetur, laborum neque repellendus
+          adipisci quae saepe vero incidunt expedita! Expedita minima eveniet
+          consequuntur laboriosam in iste sapiente.
+        </p>
+        <div className="policies">
+          <span>
+            <FontAwesomeIcon icon={faArrowRight} />
+            <a
+              id="linkanimation"
+              href="https://merchant.razorpay.com/policy/OdiFBga4sh8941/privacy"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Privacy Policy
+            </a>
+          </span>
+          <span>
+            <FontAwesomeIcon icon={faArrowRight} />
+            <a
+              id="linkanimation"
+              href="https://merchant.razorpay.com/policy/OdiFBga4sh8941/refund"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Cancellation and Refund Policy
+            </a>
+          </span>
+          <span>
+            <FontAwesomeIcon icon={faArrowRight} />
+            <a
+              id="linkanimation"
+              href="https://merchant.razorpay.com/policy/OdiFBga4sh8941/terms"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Terms And Conditions
+            </a>
+          </span>
+        </div>
       </div>
-
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
