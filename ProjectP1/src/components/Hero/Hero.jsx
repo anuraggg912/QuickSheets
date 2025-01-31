a92649d988f202d81078885693abb417ac252b92
Anurag <anrg1576@gmail.com>
2025-01-31 14:55:24 +0530

    new file:   ProjectP1/src/assets/HeroBG.png 	deleted:    ProjectP1/src/assets/heroBG.svg 	modified:   ProjectP1/src/components/Hero/Hero.css 	modified:   ProjectP1/src/components/Hero/Hero.jsx

--- ProjectP1/src/components/Hero/Hero.jsx
+++ ProjectP1/src/components/Hero/Hero.jsx
@@ -1,21 +1,28 @@
-import React from 'react'
-import './Hero.css'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import './Hero.css';
+import { Link } from 'react-router-dom';
+import HeroBG from '../../assets/HeroBG.png'; 
 
 const Hero = () => {
   return (
     <div className='hero container'>
-      <div className='hero-text'>
-        <h1>Get Your Stationery Delivered for Free!</h1>
-        <p>Enjoy the convenience of free delivery on your stationery needs. From black & white, colored documents to practical files and spiral-bound reports, we've got you covered. Order now and streamline your work with our fast and reliable service!</p>
-        <Link to="/order">
-          <button className="btn">ORDER NOW</button>
-        </Link>
-        <h2>Save time by sending orders online from anywhere
-        </h2>
+
+      <div className='hero-left'>
+        <div className='hero-text'>
+          <h1>Get Your Stationery Delivered for Free!</h1>
+          <p>Enjoy the convenience of free delivery on your stationery needs. From black & white, colored printouts to practical files and spiral-bindings, we've got you covered. Order now and streamline your work with our fast and reliable service!</p>
+          <Link to="/order">
+            <button className="btn">ORDER NOW</button>
+          </Link>
+          <h2>Save time by sending orders online from anywhere</h2>
+        </div>
+      </div>
+
+      <div className='hero-right'>
+      <img src={HeroBG} alt="Stationery Delivery" />
       </div>
     </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
